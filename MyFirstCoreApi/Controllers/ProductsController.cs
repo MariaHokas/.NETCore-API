c63ea95cda45f87fa4d8d89db3aad3c154794970
Ahokas Maria Katariina <maria.ahokas@student.careeria.fi>
2020-03-28 08:17:08 +0200

    Add project files.

---
+++ MyFirstCoreApi/Controllers/ProductsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyFirstCoreApi.Models;
+
+namespace MyFirstCoreApi.Controllers
+{
+    [Route("nw/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public List<Products> GetAllProducts()
+        {
+            NorthwindContext db = new NorthwindContext();
+            List<Products> tuotteet = db.Products.ToList();
+            return tuotteet;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public Products GetOneProduct(int id)
+        {
+            NorthwindContext db = new NorthwindContext();
+            Products tuote = db.Products.Find(id);
+            return tuote;
+        }
+
+        [HttpGet]
+        [Route("SupplierID/{key}")]
+        public List<Products> GetSomeProduct(int key)
+        {
+            NorthwindContext db = new NorthwindContext();
+
+            var SomeProducts = from c in db.Products
+                                where c.SupplierId == key
+                                select c;
+
+            return SomeProducts.ToList();
+        }
+
+        [HttpPost] //<--filtteri rajoittaa alapuolella olevan metodin vain POST-pyyntöihin (uuden asian luominen tai lisääminen)
+        [Route("")] //<--tyhjä reitinmääritys (ei ole pakko laittaa), eli ei mitään lisättävää reittiin, jolloin
+        public ActionResult PostCreateNew([FromBody] Products tuotteet) //<-- [FromBody] tarkoittaa, että HTTP-pyynnön Body:ssä välitetään JSON-muodossa oleva objekti ,joka om Documentation-tyyppinen customer-niminen
+        {
+            NorthwindContext db = new NorthwindContext();
+            try
+            {
+                db.Products.Add(tuotteet);
+                db.SaveChanges();
+
+                return Ok(tuotteet.ProductId);
+            }
+
+            catch (Exception)
+            {
+                return BadRequest("Jokin meni pieleen asiakasta lisättäessä");
+            }
+
+            finally
+            {
+                db.Dispose();
+            }
+
+            /*return doku.DocumentationId.ToString; //k*//*uittaus Frontille, että päivitys meni oikein --> Frontti voi tsekata, että kontrolleri palauttaa saman id:n mitä käsitteli*/
+        }
+
+        //[HttpPost] //<--filtteri rajoittaa alapuolella olevan metodin vain POST-pyyntöihin (uuden asian luominen tai lisääminen)
+        //[Route("")] //<--tyhjä reitinmääritys (ei ole pakko laittaa), eli ei mitään lisättävää reittiin, jolloin
+        //public string PostCreateNew([FromBody] Products tuote) //<-- [FromBody] tarkoittaa, että HTTP-pyynnön Body:ssä välitetään JSON-muodossa oleva objekti ,joka om Documentation-tyyppinen customer-niminen
+        //{
+        //    NorthwindContext context = new NorthwindContext(); //Context = Kuten entities muodostettu Scaffold DBContext -tykalulla. Voisi olla myös entiteetti frameworkCore
+        //    context.Products.Add(tuote);
+        //    context.SaveChanges();
+
+        //    return tuote.Doc.ToString(); //kuittaus Frontille, että päivitys meni oikein --> Frontti voi tsekata, että kontrolleri palauttaa saman id:n mitä käsitteli
+        //}
+
+        [HttpPut] //<-- filtteri, joka sallii vain PUT-metodit (Http-verbit)
+        [Route("{key}")] //<-- Routemääritys asiakasavaimelle key=CustomerID
+        public ActionResult PutEdit(int key, [FromBody] Products tuotteet) //<-- [FromBody] tarkoittaa, että HTTP-pyynnön Body:ssä välitetään JSON-muodossa oleva objekti ,joka on Customers-tyyppinen asiakas-niminen
+        {
+            NorthwindContext db = new NorthwindContext(); //Context = Kuten entities muodostettu Scaffold DBContext -työkalulla. Voisi olla myös entiteetti frameworkCore
+            try
+            {
+                Products product = db.Products.Find(key);
+                if (tuotteet != null)
+                {
+                    product.ProductName = tuotteet.ProductName;
+                    product.SupplierId = tuotteet.SupplierId;
+                    product.UnitPrice = tuotteet.UnitPrice;
+                    product.UnitsInStock = tuotteet.UnitsInStock;
+                    db.SaveChanges();
+                    return Ok(tuotteet.ProductId);
+                }
+
+                else
+                {
+                    return NotFound("Päivitettävää asiakasta ei löytynyt!");
+                }
+            }
+            catch (Exception)
+            {
+                return BadRequest("Jokin meni pieleen asiakasta lpäivittäessä");
+            }
+
+            finally
+            {
+                db.Dispose();
+            }
+        }
+
+        [HttpDelete]
+        [Route("{key}")]
+        public string DeleteSingle(int key)
+        {
+            NorthwindContext context = new NorthwindContext();
+            Products tuotteet = context.Products.Find(key);
+
+            if (tuotteet != null)
+            {
+                context.Products.Remove(tuotteet);
+                context.SaveChanges();
+                return "DELETED";
+            }
+            return "NOT FOUND";
+        }
+    }
+}
---
+++ MyFirstCoreApi/Models/Henkilo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstCoreApi.Models
+{
+    public class Henkilo
+    {
+        public String Nimi { get; set; }
+        public String Osoite { get; set; }
+        public int Ika { get; set; }
+    }
+}
